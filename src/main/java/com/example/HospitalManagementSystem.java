9ec878d5617eaf33267128621ef33268bfccb5d8
Arman <armanhazarikajrt@gamil.com>
2025-12-15 11:57:01 +0530

    Initial commit

---
+++ src/main/java/com/example/HospitalManagementSystem.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class HospitalManagementSystem 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Welcome to Hospital Management System!");
+    }
+}
